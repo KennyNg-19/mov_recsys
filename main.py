d2cb839e01f420b04cbc7263e3439f7aef353a11
Yuhao_Wu <wuyuhao2019@126.com>
2022-08-04 18:31:49 +0800

    feat: add exception catcher

--- main.py
+++ main.py
@@ -3,14 +3,14 @@
 Author: Yuhao_Wu wuyuhao2019@126.com
 Date: 2022-08-02 16:27:04
 LastEditors: Yuhao_Wu
-LastEditTime: 2022-08-04 14:47:41
+LastEditTime: 2022-08-04 18:14:14
 Description: The file for building Fastapi.
 '''
 
 # Python standard/ 3rd-party / local modules
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, Query
 from pprint import pprint
 
 from user_cf import *
@@ -26,7 +26,9 @@ async def root():
 
 @app.get("/rec/{user_id}")
 # async def read_item(user_id: int, k:int):
-async def read_item(user_id: int, k: Union[int, None] = 10):
+# declare additional information and validation for input parameters.
+async def read_item(user_id: int = Path(title="The ID of target user"),
+                    k: Union[int, None] = 10):
     """given a user, output top K predicted movies as recommendations
 
     Args:
@@ -36,7 +38,12 @@ async def read_item(user_id: int, k: Union[int, None] = 10):
     Returns:
         a list of tuple: movie recommendations with scores
     """
-    movies_rec = rec_mov(user_id, k)
+    try:
+        user_id = int(user_id)
+    except ValueError:
+        print("Not an integer! Try again.")
+    else:
+        movies_rec = rec_mov(user_id, k)
 
-    return {f"top{k}_recmmond_movs": movies_rec}
-    # return json.dumps(movies_rec)
+        return {f"top{k}_recmmond_movs": movies_rec}
+        # return json.dumps(movies_rec)
